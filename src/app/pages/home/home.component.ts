52729694c211b43ce39af0c287daf0c5a8126dd6
Cibele Martins <cibelemartins24@hotmail.com>
2023-02-19 15:09:38 -0300

    🚧 ➕ build: add angular material, e implementou reponsividade c/ observável

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,7 +9,30 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  flexDirectionColumn:boolean = false
+
+  constructor(private breakpointService: BreakpointObserver){
+
+  }
   closeConversor() {
-    
+    // this.breakpointService.observe(Breakpoints.Small).subscribe((result)=> {
+
+    //   this.flexDirection = ''
+    //   if(result.matches){
+    //     this.flexDirection = 'column'
+    //   }
+    // })
+
+    this.breakpointService.observe([Breakpoints.Small, Breakpoints.XSmall]).subscribe((result)=> {
+
+      // this.flexDirectionColumn = false;
+      if(result.breakpoints[Breakpoints.Small]){
+        this.flexDirectionColumn = true;
+      }
+
+      if(result.breakpoints[Breakpoints.XSmall]){
+        this.flexDirectionColumn = true;
+      }
+    })
   }
 }
