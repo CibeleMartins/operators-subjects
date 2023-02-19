fbd7b9d2df5df1a49afea1c42fe09f32a7f74e92
Cibele Martins <cibelemartins24@hotmail.com>
2023-02-19 12:05:49 -0300

    🚧 feat: implementou o gráfico

--- src/app/components/graphic/graphic.component.ts
+++ src/app/components/graphic/graphic.component.ts
@@ -1,14 +1,34 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Chart } from 'chart.js';
 @Component({
   selector: 'app-graphic',
   templateUrl: './graphic.component.html',
   styleUrls: ['./graphic.component.scss']
 })
 export class GraphicComponent implements OnInit {
+  @ViewChild('graphic', { static: true })
+  refGraphic!: ElementRef;
 
   ngOnInit() {
-    
+    new Chart(this.refGraphic.nativeElement, {
+      type: 'line',
+      data: {
+        labels: ['um titulo pra cada valor'],
+        datasets: [
+          {data: ['valores a exibir no gráfico'],
+          borderColor: 'cor da linha ',
+          fill: false},
+        
+          {data: ['outros valores'],
+          borderColor: 'cor da linha ',
+          fill: false,},
+          
+          {data: ['outros valores'],
+          borderColor: 'cor da linha ',
+          fill: false,}
+        ]
+      },
+    })
   }
 
 }
