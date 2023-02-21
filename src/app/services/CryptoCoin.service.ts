2ae4ca6f65027b8b5b25f4fee06b73bb11bdb43a
Cibele Martins <cibelemartins24@hotmail.com>
2023-02-21 18:38:56 -0300

    🚧 feat: estilizou e implementou a home, o comp. da valores e o painel de infors de cryptomoedas

--- src/app/services/CryptoCoin.service.ts
+++ src/app/services/CryptoCoin.service.ts
@@ -6,7 +6,7 @@ export interface CryptoInfo {
     symbol: string,
     name: string,
     buyValue: number,
-    priceVariationPercentage: string
+    priceVariationPercentage: number
 }
 
 @Injectable({ providedIn: 'root' })
@@ -21,7 +21,7 @@ export class CryptoCoinService {
     getCryptoInfos() {
         return this.http.get('https://api1.binance.com/api/v3/ticker/24hr').pipe(map((i) => {
             Object.values(i).filter((i) => i.symbol.includes('BRL') ? i : '').map((i) => {
-                this.cryptosInfos.push(this.crypto = { symbol: '', name: i.symbol, buyValue: i.bidPrice, priceVariationPercentage: i.priceChangePercent })
+                this.cryptosInfos.push(this.crypto = { symbol: '', name: i.symbol, buyValue:parseFloat( parseFloat(i.bidPrice).toFixed(2)), priceVariationPercentage: parseFloat( parseFloat(i.priceChangePercent).toFixed(2)) })
             })
             return this.cryptosInfos
         }))
