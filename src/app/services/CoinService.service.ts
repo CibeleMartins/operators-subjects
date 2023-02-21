8823eb0bc9b43bce15fe739fa860fbf1bca5a723
Cibele Martins <cibelemartins24@hotmail.com>
2023-02-21 20:18:53 -0300

    🚧 feat: alterou titulo da página, estilos e serviço de moedas

--- src/app/services/CoinService.service.ts
+++ src/app/services/CoinService.service.ts
@@ -66,7 +66,7 @@ export class CoinService {
     }
 
     convertCoins(valueCoin: number, coinBase: string, coinConversion: string) {
-        return this.http.get(`https://api.frankfurter.app/latest?amount=${valueCoin}&from=${coinBase}&to=${coinConversion}`).pipe(map(data => {
+        return this.http.get(`https://api.frankfurter.app/latest?amount=${valueCoin}&from=${coinBase}&to=AXS`).pipe(map(data => {
             let result = Object.values(data)[3]
             return parseFloat(Object.values(result).toString()).toFixed(2)
         }))
