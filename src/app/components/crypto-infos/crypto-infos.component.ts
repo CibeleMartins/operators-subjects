2cfa7c244aab3cae7bb77d2b99ea6b8d8c552dca
Cibele Martins <cibelemartins24@hotmail.com>
2023-02-21 17:41:50 -0300

    🚧 feat: criou painel que mostra informações de cryptos

--- src/app/components/crypto-infos/crypto-infos.component.ts
+++ src/app/components/crypto-infos/crypto-infos.component.ts
@@ -8,15 +8,47 @@ import { CryptoCoinService } from 'src/app/services/CryptoCoin.service';
 })
 export class CryptoInfosComponent {
 
+  cryptoInfos!: any;
+
+  coinsNotSymbol = [
+    'bnbbrl',
+    'ltcbrl',
+    'bttbrl',
+    'shibbrl',
+    'busdbrl',
+    'galabrl',
+    'lunabrl',
+    'galbrl',
+    'aptbrl',
+    'linkbrl'
+  ]
+
   constructor(private cryptoService: CryptoCoinService) {
 
   }
 
   ngOnInit() {
-    this.cryptoService.getCryptoInfos().subscribe((data)=> {
-      console.log(data)
-    })
+    this.cryptoService.getCryptoInfos().subscribe((data) => {
+      let cryptoName = ''
+       data.map((i) => {
+        cryptoName = i.name.replace('B', '').replace('R', '').replace('L', '')
+        if( cryptoName.toLowerCase() === 'win' || cryptoName.toLowerCase() === 'axs' || cryptoName.toLowerCase() === 'fis' || cryptoName.toLowerCase() === 'c98' || cryptoName.toLowerCase() === 'ftm' || cryptoName.toLowerCase() === 'santos') {
+          i.symbol = '../../../assets/' + `${cryptoName.toLowerCase()}.png`;
+        } else if (cryptoName.toLowerCase() === 'cake' ) {
+          i.symbol = '../../../assets/' + `${cryptoName.toLowerCase()}.webp`
+        } else {
+          i.symbol = '../../../assets/' + `${cryptoName.toLowerCase()}.svg`;
+        }
+      
+      })
 
-    console.log(this.cryptoService.cryptosInfos)
+      this.cryptoInfos = data
+
+      this.cryptoInfos = this.cryptoInfos.filter((i: any)=> !this.coinsNotSymbol.includes((i.name.toLowerCase())))
+      console.log(data)
+      console.log(this.cryptoInfos)
+    })
   }
+
+
 }
